dd586a295992f5fe0e47b51b36498babff3e20c6
Leylim <leylimyalcin@hotmail.com>
2018-06-29 10:55:13 +0300

    Add files via upload

---
+++ get_emp_name.sql
@@ -0,0 +1,10 @@
+/*
+*  Author: Leylim Yalcin 27.06.2018
+*  BEGIN-END block for executing the package
+*  
+*/
+@MY_STAJ_UTULITY.pks
+@MY_STAJ_UTULITY.pkb
+BEGIN
+MY_STAJ_UTULITY.employee_name(101);
+END;
